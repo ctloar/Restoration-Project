7e0371c82a8d408d85def591e8e23c3b73ea37fd
ctloar <cl464@byu.edu>
2023-04-03 13:13:07 -0600

    remove json for testing

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // This example displays a marker at the center of Australia.
 // When the user clicks the marker, an info window opens.
-import json from "./descriptions.json";
 
 function initMap() {
   const sacred_grove = { lat: 43.04067395742267, lng: -77.24432780452317 };
@@ -15,15 +14,18 @@ function initMap() {
     '<div id="content">' +
     '<div id="siteNotice">' +
     "</div>" +
-    '<h1 id="firstHeading" class="firstHeading">' +
-    json.sacred_grove[0] +
-    "</h1>" +
+    '<h1 id="firstHeading" class="firstHeading">The Sacred Grove</h1>' +
     '<div id="bodyContent">' +
-    "<p><b>" +
-    json.sacred_grove[0] +
-    "</b>" +
-    json.sacred_grove[1] +
-    "</p>" +
+    "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+    "sandstone rock formation in the southern part of the " +
+    "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+    "south west of the nearest large town, Alice Springs; 450&#160;km " +
+    "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+    "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+    "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+    "Aboriginal people of the area. It has many springs, waterholes, " +
+    "rock caves and ancient paintings. Uluru is listed as a World " +
+    "Heritage Site.</p>" +
     '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
     "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
     "(last visited June 22, 2009).</p>" +
